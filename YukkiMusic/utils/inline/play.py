88703f6c856c68bb366b41dc64f41d6a64abb391
aabbddooelsyed <111532118+aabbddooelsyed@users.noreply.github.com>
2022-08-23 11:35:22 +0200

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -57,8 +57,8 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text=_["SOURCE_DRAGON"],
-                url=f"https://t.me/yy8gg",
+                text=_["⸢𝙎𝙊𝙐𝙍𝘾𝙀 𝘽𝘼𝘽𝙇𝙊𝙊⸥"],
+                url=f"https://t.me/BA_BLOO",
             ),
         ],
     ]
